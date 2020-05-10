f0ea25293965f0258144de4f47ca108259685c99
Administrator <peter_gits@dell.com>
2020-05-09 19:14:05 -0500

    working properly

--- Siri-Intents-Example/AppDelegate.swift
+++ Siri-Intents-Example/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -20,12 +20,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
+    @available(iOS 13.0, *)
+    @available(iOS 13.0, *)
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    func application(_ application: UIApplication, continue userActivity:NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        print("UserActivityType is \(userActivity.activityType)")
+        if(userActivity.activityType == "com.geekgaps.Siri.Siri-Intents-Example.OrderBigMacPlus"){
+            if let vc = window?.rootViewController as? ViewController {
+                if let lblOrder = vc.OrderLbl {
+                    lblOrder.text = "Ordering a BigMac, Fries, & Coke" //\(userActivity.activityType)"
+                }
+            }
+        }
+        return false
+    }
+        
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
--- Siri-Intents-Example/ViewController.swift
+++ Siri-Intents-Example/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+//com.geekgaps.Siri.Siri-Intents-Example
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,6 +16,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var OrderLbl: UILabel!
+    
+    @IBAction func OrderBigMacPlus(_ sender: Any) {
+        print("Order BigMac Plus")
+        let activity = NSUserActivity(activityType: "com.geekgaps.Siri.Siri-Intents-Example.OrderBigMacPlus")
+        activity.title = "Order BigMac, Fries, and Coke"
+        activity.isEligibleForSearch = true
+        activity.isEligibleForPrediction = true
+        self.userActivity = activity
+        self.userActivity?.becomeCurrent()
+    }
 }
 
